0929ce0215ce13d2ac5744576a40b659d5bef9cc
Gerrit Klein <therealthingy95@gmail.com>
2025-05-22 15:25:06 +0200

    chore: ch13 lowercase filenames

--- ch13/SINT.C
+++
@@ -1,8 +0,0 @@
-/*
-** The proper usage and copyright information for
-** this software is covered in DSCRLic.TXT
-** This code is Copyright 1999 by Dann Corbit
-*/
-
-#define E_TYPE_SIGNED_INT
-#include "allsort.h"
